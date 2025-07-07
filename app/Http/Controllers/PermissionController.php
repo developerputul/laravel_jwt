883c9da5adb7186bd7f0c816e01585cf2db78c83
developerputul <putulsentongchangya7@gmail.com>
2025-07-08 00:38:40 +0600

    Permission Create and index Section updated

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('permissions.index');
+    } //End Method
+    public function create()
+    {
+        return view('permissions.create');
+    } //End Method
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:permissions|min:3',
+        ]);
+
+        if ($validator->passes()) {
+            Permission::create(['name' => $request->name]);
+            return redirect()->route('permissions.index')->with('success', 'Permission added Successfully');
+        } else {
+            return redirect()->route('permissions.create')->withInput()->withErrors($validator);
+        }
+    } //End Method
+    public function edit()
+    {
+        //
+
+    } //End Method
+    public function update()
+    {
+        //
+
+    } //End Method
+    public function delete()
+    {
+        //
+
+    } //End Method
 }
